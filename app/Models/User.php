1846e2550ec0b05999e04e049e9b76a2f5c949db
Sesita <iraklisesit@gmail.com>
2024-03-07 18:56:47 +0400

    fix: bug

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
