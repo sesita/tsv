a338742810367f3ac771a602cbb9497ce5a9e8ea
Sesita <iraklisesit@gmail.com>
2024-04-03 17:01:55 +0400

    upd

--- react-app/src/pages/Upload/UploadPage.js
+++ react-app/src/pages/Upload/UploadPage.js
@@ -7,11 +7,7 @@ const SettingsPage = () => {
     return (
         <>
             <UserLayout pageTitle={"Upload New Video 🥳"}>
-                <div className="flex items-center justify-center">
-                    <PriceTable tableId={"prctbl_1OtHkRB9uNXBCzh8JtU0CaoD"} />
-                    <PriceTable tableId={"prctbl_1OtHUHB9uNXBCzh8U8U7xBYY"} />
-                    <PriceTable tableId={"prctbl_1OtHpwB9uNXBCzh8TSnkzLmQ"} />
-                </div>
+                    <PriceTable tableId={"prctbl_1OtW9ZB9uNXBCzh8wvxphwYE"} className="col-span-1" />
                 <Upload />
             </UserLayout>
         </>
