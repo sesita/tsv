6abcbe2f653780392e8878481cbfd6ec2a90e428
Sesita <iraklisesit@gmail.com>
2024-07-09 14:00:46 +0400

    fix: build directory

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    root: 'react-app',
-    plugins: [react()]
+    root: "react-app",
+    build: {
+        outDir: "../public",
+        emptyOutDir: false,
+    },
+    plugins: [react()],
 });
