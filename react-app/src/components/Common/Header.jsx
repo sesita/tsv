6c951a56deb581fff4f136ed3dbea2ee7df5f0fe
Sesita <iraklisesit@gmail.com>
2024-08-15 23:00:22 +0400

    fix: pages

--- react-app/src/components/Common/Header.jsx
+++ react-app/src/components/Common/Header.jsx
@@ -14,10 +14,10 @@ import { BiSolidVideoPlus } from "react-icons/bi";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { useState, useRef, useEffect } from "react";
 import { useAuth } from "../../context/AuthContext";
-import { IoLocationOutline } from "react-icons/io5";
+import { IoHomeOutline, IoLocationOutline } from "react-icons/io5";
 import { Tooltip as ReactTooltip } from "react-tooltip";
 import { BsChevronUp, BsGraphUpArrow } from "react-icons/bs";
-import { IoMdClose, IoMdNotifications } from "react-icons/io";
+import { IoMdClose, IoMdNotifications, IoMdNotificationsOutline } from "react-icons/io";
 import { AiFillPlayCircle, AiFillSetting } from "react-icons/ai";
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import { useDetectClickOutside } from "react-detect-click-outside";
@@ -154,130 +154,16 @@ const Header = ({ searchQuery }) => {
 
     return (
         <>
-            <div className={`fixed top-0 left-0 w-full h-full bg-white z-30 transition-transform duration-300 ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"} md:hidden`}>
-                <div className="flex justify-between items-center p-4 border-b">
-                    <Link to={"/"}>
-                        <img src="/assets/logo.png" alt="Logo" className="max-w-[120px]" />
-                    </Link>
-                    <button onClick={toggleSidebar} className="text-red-700 text-2xl">
-                        <IoMdClose className="text-3xl" />
-                    </button>
-                </div>
-                <div className="p-4">
-                    <div className="flex flex-col gap-2 mb-4">
-                        {currentUser && (
-                            <div className="flex justify-between items-center border-b mb-2 pb-4">
-                                <div className="flex">
-                                    <img src={currentUser?.avatar} className="w-12 h-12 rounded-full mr-3 object-cover" />
-                                    <div className="block text-dark-white">
-                                        <p className="text-sm text-gray-700"> Welcome, Back </p>
-                                        <p className="text-lg font-medium">Test </p>
-                                    </div>
-                                </div>
-                                <Link to={"/User/Profile"} className="h-full text-2xl">
-                                    <BsChevronUp className="text-primary rotate-90" />
-                                </Link>
-                            </div>
-                        )}
-                        <label className="font-medium text-2xl text-gray-600 items-center mx-1 flex gap-2 cursor-pointer mb-2" data-tooltip-id="location">
-                            <IoLocationOutline className="text-primary" />
-                            Location
-                            <FaInfoCircle className="mt-1 text-sm" />
-                        </label>
-                        <ReactTooltip id="location" content="For Show Related Videos" />
-                        <div className="flex gap-4 w-full">
-                            <div className="flex-1">
-                                <Select
-                                    options={countryCityData}
-                                    onChange={handleCountryChange}
-                                    placeholder="State"
-                                    classNamePrefix="react-select"
-                                    styles={{
-                                        control: (provided) => ({
-                                            ...provided,
-                                            borderRadius: "1rem",
-                                            padding: "0.3rem 0.5rem",
-                                            outline: "none",
-                                            fontWeight: "500",
-                                            width: "100%", // Ensure full width
-                                            boxSizing: "border-box",
-                                        }),
-                                        placeholder: (provided) => ({
-                                            ...provided,
-                                            color: "#6b7280",
-                                        }),
-                                    }}
-                                />
-                            </div>
-                            <div className="flex-1">
-                                <Select
-                                    options={cityOptions}
-                                    isDisabled={!selectedCountry}
-                                    onChange={handleCityChange}
-                                    placeholder="City"
-                                    classNamePrefix="react-select"
-                                    styles={{
-                                        control: (provided) => ({
-                                            ...provided,
-                                            borderRadius: "1rem",
-                                            padding: "0.3rem 0.5rem",
-                                            outline: "none",
-                                            fontWeight: "500",
-                                            width: "100%", // Ensure full width
-                                            boxSizing: "border-box",
-                                        }),
-                                        placeholder: (provided) => ({
-                                            ...provided,
-                                            color: "#6b7280",
-                                        }),
-                                    }}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    {currentUser && (
-                        <div className="flex flex-col p-2 gap-6 text-2xl font-medium text-gray-600 border-t mt-6 pt-6">
-                            <Link to={`/User/Profile`} className="flex items-center gap-3">
-                                <CgProfile className="text-primary" />
-                                Profile
-                            </Link>
-                            <Link to={`/User/Profile`} className="flex items-center gap-3">
-                                <IoMdNotifications className="text-primary" />
-                                Notifications
-                            </Link>
-                            <Link to={`/User/Videos`} className="flex items-center gap-3">
-                                <AiFillPlayCircle className="text-primary" />
-                                My Videos
-                            </Link>
-                            <Link to={`/User/Analytics`} className="flex items-center gap-3">
-                                <BsGraphUpArrow className="text-primary" />
-                                Analytics
-                            </Link>
-                            <Link to={`/User/Upload`} className="flex items-center gap-3">
-                                <CgProfile className="text-primary" />
-                                Promotion
-                            </Link>
-                            <Link to={`/User/Settings`} className="flex items-center gap-3 border-b pb-6">
-                                <AiFillSetting className="text-primary" />
-                                Settings
-                            </Link>
-                            <button className="flex items-center gap-3" onClick={() => logout({})}>
-                                <VscSignOut className="text-primary" />
-                                Sign Out
-                            </button>
-                        </div>
-                    )}
-                </div>
-            </div>
-
             <div className="sticky top-0 z-20">
                 <div className={`bg-white transition-all duration-300 ${!showCategories && "shadow-[0px_5px_10px_0px_rgba(0,0,0,0.1)]"}`}>
                     <header className="container flex justify-between items-center py-5">
+                        {/* Logo Section */}
                         <Link to="/">
                             <img src="/assets/logo.png" alt="Logo" className="w-full hidden md:inline max-w-[150px]" />
                             <img src="/assets/short-logo.png" alt="Logo" className="w-full md:hidden max-w-[50px]" />
                         </Link>
 
+                        {/* Search Bar Section */}
                         <div className="w-2/3 md:w-1/2 px-4">
                             <form className="w-full rounded-full border-[1px] border-[#CACACA] py-1 px-1 flex" onSubmit={handleSearch}>
                                 <input type="text" className="outline-none border-none md:text-sm text-xs sm:pl-4 pl-2 flex-1 rounded-lg" placeholder="Search..." value={searchQuery || searchText} onChange={(e) => setSearchText(e.target.value)} />
@@ -287,6 +173,7 @@ const Header = ({ searchQuery }) => {
                             </form>
                         </div>
 
+                        {/* User & Notification Section */}
                         {currentUser ? (
                             <div className="flex gap-4 items-center relative">
                                 <div className="hidden md:flex gap-4 items-center relative">
@@ -296,7 +183,6 @@ const Header = ({ searchQuery }) => {
                                     <div className="relative cursor-pointer" ref={notificationRef} onClick={() => setNotificationDropdown(!notificationDropdown)}>
                                         <span className="absolute border-[2px] border-white rounded-full w-5 h-5 flex justify-center items-center bg-[#C60C0D] text-white text-[11px] -top-1.5 -right-1.5">0</span>
                                         <IoMdNotifications className="md:text-3xl text-xl" />
-
                                         {notificationDropdown && (
                                             <div className="shadow-[0px_0px_5px_0px_rgba(0,0,0,0.2)] rounded-xl py-4 px-4 absolute right-0 top-12 w-52 z-20 text-center bg-white">
                                                 <span className="font-medium text-xl text-red-600 capitalize">Notifications</span>
@@ -312,7 +198,6 @@ const Header = ({ searchQuery }) => {
                                         <img src={currentUser.avatar} className="rounded-full w-8 h-8 mr-2" alt={currentUser.name} />
                                         <BsChevronUp className={!showDropdown && "rotate-180 hidden md:block"} />
                                     </div>
-
                                     <div className={`animate__animated animate__fadeIn shadow-[0px_0px_14px_0px_rgba(0,0,0,0.2)] rounded-xl py-4 px-5 absolute right-0 top-12 w-48 z-20 bg-white ${!showDropdown && "hidden"} `}>
                                         <span className="font-medium text-xl text-red-600 capitalize">{currentUser.name}</span>
                                         <hr className="my-2.5" />
@@ -387,6 +272,24 @@ const Header = ({ searchQuery }) => {
                         )}
                     </header>
                 </div>
+
+                {/* Mobile Icons Section */}
+                <div className="md:hidden flex justify-around items-center py-3 bg-gray-100 border-t">
+                    <Link to="/home">
+                        <IoHomeOutline className="text-2xl text-primary" />
+                    </Link>
+                    <Link to="/search">
+                        <CiSearch className="text-2xl text-primary" />
+                    </Link>
+                    <Link to="/notifications">
+                        <IoMdNotificationsOutline className="text-2xl text-primary" />
+                    </Link>
+                    <Link to="/profile">
+                        <CgProfile className="text-2xl text-primary" />
+                    </Link>
+                </div>
+
+                {/* Categories Section */}
                 <div className={`bg-white border-t shadow-[0px_5px_10px_0px_rgba(0,0,0,0.1)] z-30 md:rounded-b-2xl transition-all duration-300 ${showCategories ? "translate-y-0 pointer-events-auto" : "opacity-0 -translate-y-full pointer-events-none"}`}>
                     <div className="sm:container md:py-4 py-3 overflow-hidden">
                         <div className="flex md:gap-x-4 pl-2 sm:pl-0 gap-x-2 md:text-lg text-sm whitespace-nowrap">{renderCategoryLinks()}</div>
